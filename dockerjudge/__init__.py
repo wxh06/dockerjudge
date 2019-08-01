d886448cc3a04f1a24dbd2055177d067ff1c1035
汪心禾 <wangxinhe06@gmail.com>
2019-08-01 16:15:27 +0800

    PEP8

--- dockerjudge/__init__.py
+++ dockerjudge/__init__.py
@@ -1,7 +1,5 @@
 'dockerjudge - A Docker Based Online Judge Engine'
 
-__version__ = '0.3.1'
-
 import shlex
 import sys
 import threading
@@ -9,10 +7,13 @@ import threading
 import docker
 import ruamel.yaml
 
+__version__ = '0.3.1'
+
 
 class Thread(threading.Thread):
     'Subclass of threading.Thread with return value'
     return_value = None
+
     def run(self):
         try:
             if self._target:
@@ -20,12 +21,14 @@ class Thread(threading.Thread):
         finally:
             del self._target, self._args, self._kwargs
 
+
 def _judge(container, commands, stdio, timeout=1):
     'Run each test case'
     if commands[0]:
         container.exec_run(commands[0])
-    result = container.exec_run(['sh', '-c', 'echo ' + shlex.quote(stdio[0]) + ' | timeout -k '
-                                 + (str(timeout) + ' ') * 2 + commands[1]], demux=True)
+    result = container.exec_run(['sh', '-c', 'echo ' + shlex.quote(stdio[0]) +
+                                 ' | timeout -k ' + (str(timeout) + ' ') * 2 +
+                                 commands[1]], demux=True)
     if commands[2]:
         container.exec_run(commands[2])
     if result.exit_code == 124:
@@ -36,12 +39,14 @@ def _judge(container, commands, stdio, timeout=1):
         return 'WA'
     return 'AC'
 
+
 def judge(settings, source='', tests=(), timeout=1, client=docker.from_env()):
     'Main judge function'
     container = client.containers.run(settings['image'], detach=True,
                                       network_disabled=True, tty=True)
     try:
-        container.exec_run(['sh', '-c', 'echo ' + shlex.quote(source) + ' > ' + settings['source']])
+        container.exec_run(['sh', '-c', 'echo ' + shlex.quote(source) +
+                            ' > ' + settings['source']])
         if 'before_compile' in settings:
             container.exec_run(settings['before_compile'])
         compiler = container.exec_run(settings['compile'], demux=True)
@@ -50,12 +55,13 @@ def judge(settings, source='', tests=(), timeout=1, client=docker.from_env()):
         if not compiler.exit_code:
             threads = []
             for stdin, stdout in tests:
-                threads.append(Thread(target=_judge,
-                                      args=(container, (settings.get('before_judge'),
-                                                        settings['judge'],
-                                                        settings.get('after_judge')),
-                                            (stdin, stdout), timeout)))
-                threads[-1].start()
+                thread = Thread(target=_judge,
+                                args=(container, (settings.get('before_judge'),
+                                                  settings['judge'],
+                                                  settings.get('after_judge')),
+                                      (stdin, stdout), timeout))
+                thread.start()
+                threads.append(thread)
             result = []
             for thread in threads:
                 thread.join()
@@ -65,6 +71,8 @@ def judge(settings, source='', tests=(), timeout=1, client=docker.from_env()):
     finally:
         container.remove(force=True)
 
+
 if __name__ == '__main__':
-    print(judge(ruamel.yaml.YAML().load(open('settings.yaml'))[sys.argv[1]][sys.argv[2]],
+    print(judge(ruamel.yaml.YAML().load(open('settings.yaml'))
+                [sys.argv[1]][sys.argv[2]],
                 sys.stdin.read(), zip(sys.argv[3::2], sys.argv[4::2])))
