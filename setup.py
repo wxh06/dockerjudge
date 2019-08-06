55dbef29f66c4f3a5fa0c12e25c26388a15c8fb5
汪心禾 <wangxinhe06@gmail.com>
2019-08-06 15:39:23 +0800

    Update setup.py according to PEP8
    pycodestyle setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setuptools.setup(
     author='汪心禾',
     author_email='wangxinhe06@gmail.com',
     description='A Docker Based Online Judge Engine',
-    long_description=open('README.md', 'r').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/wangxinhe2006/dockerjudge',
-    packages=setuptools.find_packages(),
+    packages=['dockerjudge'],
     install_requires=[
         'docker[tls]>=3.7',
         'ruamel.yaml'
@@ -21,7 +21,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
     ],
 )
