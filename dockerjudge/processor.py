ea475676427f3998ee6331211eb8620b7a27fc49
汪心禾 <wangxinhe06@gmail.com>
2020-06-14 13:18:49 +0800

    dockerjudge.processor.GCC._get_language

--- dockerjudge/processor.py
+++ dockerjudge/processor.py
@@ -9,7 +9,7 @@ class Processor():
     'Defines the operations of a multi-version programming language processor'
 
     @staticmethod
-    def _get_image_tag(image, tag):
+    def _get_image_with_tag(image, tag):
         return image + (f':{tag}' if tag else '')
 
     image = None
@@ -31,16 +31,19 @@ class GCC(Processor):
         c = 'C'
         cpp = 'C++'
 
-    def __init__(self, language=None, version=None,
-                 filenames=None, options=None):
-        lang = (language if isinstance(language, self.Language)
+    def _get_language(self, language):
+        return (language if isinstance(language, self.Language)
                 else self.Language[language] if language in ['c', 'cpp']
                 else self.Language(language) if language in ['C', 'C++']
                 else self.Language.cpp)
+
+    def __init__(self, language=None, version=None,
+                 filenames=None, options=None):
+        lang = self._get_language(language)
         fns = filenames or {}
         args = options or []
 
-        self.image = self._get_image_tag('gcc', version)
+        self.image = self._get_image_with_tag('gcc', version)
         self.source = fns.get('src', f'a.{lang.name}')
         self.compile = ([{self.Language.c: 'gcc',
                           self.Language.cpp: 'g++'}[lang],
