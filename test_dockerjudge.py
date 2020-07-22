f6e07a0c452318725740756fd7a7605eac80dd27
汪心禾 <wangxinhe06@gmail.com>
2020-07-22 13:45:25 +0800

    test_TLE .8s

--- test_dockerjudge.py
+++ test_dockerjudge.py
@@ -99,11 +99,11 @@ class TestDockerJudge(unittest.TestCase):
                 }
             ''',
             [(b'1 1', b'1'), (b'0 0', b'')],
-            {'limit': {'time': .5}}
+            {'limit': {'time': .8}}
         )
         self.assertEqual(result[0][0][0], Status.TLE)
         self.assertEqual(result[0][1][0], Status.RE)
-        self.assertAlmostEqual(result[0][0][2], .5, 0)
+        self.assertAlmostEqual(result[0][0][2], .8, 0)
         self.assertAlmostEqual(result[0][1][2], .0, 0)
         self.assertFalse(result[1])
 
